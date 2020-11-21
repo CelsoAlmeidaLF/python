ccc04e5693428326514a41344e3baccfacfe63ae
CelsoAlmeidaLF <celso.almeida.leite@hotmail.com>
2020-11-21 17:17:09 -0300

    UPDATE Project Startup.

--- app/modules/dao.py
+++ app/modules/dao.py
@@ -1,3 +1,4 @@
+import sqlite3
 class DAO:
     def __init__(self):
         self.__init__
@@ -5,4 +6,16 @@ class DAO:
 
     def connect(self):
         str = "conection ..."
+
+        self.mydb = sqlite3.connect('db.sqlite')
+        cmd = self.mydb.cursor()
+
+        # cmd.execute("CREATE TABLE CLIENT ( Nome Text, Idade Integer, email Text )")
+        # cmd.execute("INSERT INTO CLIENT VALUES (
+        #       'Nathalia', 22, 'celso.almeida.leite@hotmail.com')")
+        # self.mydb.commit()
+
+        cmd.execute("SELECT * FROM CLIENT")
+        print(cmd.fetchall())
+
         return str
