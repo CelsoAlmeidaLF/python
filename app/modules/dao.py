865780cf8278ba32c09a1961048144fd2087c6d5
CelsoAlmeidaLF <celso.almeida.leite@hotmail.com>
2020-11-21 18:02:10 -0300

    UPDATE Project Startup.

--- app/modules/dao.py
+++ app/modules/dao.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 class DAO:
     def __init__(self):
         self.__init__
@@ -8,22 +9,44 @@ class DAO:
             self.mydb = sqlite3.connect('db.sqlite')
             
     def Set(self, xcmmd):
-        self.connect()
-        cmmd = self.mydb.cursor()
-        cmmd.execute(xcmmd)
-        cmmd.execute("SELECT * FROM CLIENT WHERE Nome = 'Nathalia'")
-        print(cmmd.fetchall())
+        try:
+            self.connect()
+            cmmd = self.mydb.cursor()
+            cmmd.execute(xcmmd)
+            cmmd.execute("SELECT * FROM CLIENT WHERE Nome = 'Nathalia'")
+            print(cmmd.fetchall())
+        except self.mydb.Error as sqlex:
+            print("ERRO!: %s" % sqlex)
         pass
 
     def Get(self):
-        self.connect()
-        cmmd = self.mydb.cursor()
-        cmmd.execute("SELECT * FROM CLIENT")
-        print(cmmd.fetchall())
+        try:
+            self.connect()
+            cmmd = self.mydb.cursor()
+            cmmd.execute("SELECT * FROM CLIENT")
+            print(cmmd.fetchall())
+        except self.mydb.Error as sqlex:
+            print("ERRO!: %s" % sqlex)
         pass
 
     def Up(self):
+        try:
+            self.connect()
+            cmmd = self.mydb.cursor()
+            cmmd.execute("UPDATE AS")
+            cmmd.execute("SELECT * FROM CLIENT WHERE Nome = 'Nathalia'")
+            print(cmmd.fetchall())
+        except self.mydb.Error as sqlex:
+            print("ERRO!: %s" % sqlex)
         pass
 
-    def Del(self):
+    def Del(self, arg):
+        try:
+            self.connect()
+            cmmd = self.mydb.cursor()
+            cmmd.execute("DELETE CLIENT WHERE Id = %s", arg)
+            cmmd.execute("SELECT * FROM CLIENT WHERE Nome = 'Nathalia'")
+            print(cmmd.fetchall())
+        except self.mydb.Error as sqlex:
+            print("ERRO!: %s" % sqlex)
         pass
